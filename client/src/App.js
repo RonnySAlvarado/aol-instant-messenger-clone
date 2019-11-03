c22bed3a2a7452cc2d4ff6fb99ef74a078eb5a12
unknown <Rsalvarado777@gmail.com>
2019-11-03 13:22:09 -0600

    Created the top section of the aol instant messenger clone with aol logo and minimize, maximize, and close icons (no functionality)

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// NPM packages
+import React from "react";
+import styled from "styled-components";
+
+// React Components
+import TopSection from "./TopSection";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <TopSection />
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  margin: 0 auto;
+  border: 2px solid blue;
+  width: 500px;
+  height: 500px;
+`;
---
+++ client/src/TopSection.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+const TopSection = () => {
+  return (
+    <TopSectionContainer>
+      <img src="/aol-logo-running-man.png" alt="aol-logo" />
+      <ButtonContainer>
+        <button>_</button>
+        <button>⧠</button>
+        <button>X</button>
+      </ButtonContainer>
+    </TopSectionContainer>
+  );
+};
+
+export default TopSection;
+
+const TopSectionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 5%;
+  background: blue;
+  align-items: center;
+  padding: 5px 5px 5px 0;
+  img {
+    width: 30px;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  height: 80%
+  padding: 0;
+  background-color: blue;
+  button {
+      border-radius: 3px;
+      border: 1px solid white;
+      background: blue;
+      color: white;
+      &:last-child {
+          background: red;
+      }
+  }
+`;
